ea743f349c1b64475f7154bfb39bbd13f16b9602
MatheusDyzarz <contatosmatheus06@gmail.com>
2026-02-11 02:00:22 -0300

    tabela
    Refactor state list and add state standardization function.

--- javascript.js
+++ javascript.js
@@ -22,28 +22,69 @@ var contadorDeEstados = 0
 
 let botao = document.getElementById("enviar")
 
+var lista = ["acre", "alagoas", "amapa", "amazonas", "bahia", "ceara", "distrito federal", "espirito santo", "goias", "maranhao", "mato grosso", "mato grosso do sul", "minas gerais",
+    "para", "parana", "paraiba", "pernambuco", "piaui", "rio grande do sul", "rio de janeiro", "rio grande do norte", "rondonia",
+    "roraima", "sao paulo", "santa catarina", "sergipe", "tocantins"];
+
+var listaAcertos = []
+
+
+
 botao.onclick = function enviarMensagem() {
     let mensagem = input.value;
     verificarEstado(mensagem)
     input.value = ""
+    for (let i=0; i < listaAcertos.length; i++) {
+    }
+    
 }
 
 
-lista = ["acre", "alagoas", "amapa", "amazonas", "bahia", "ceara", "distrito federal", "espirito santos", "goias", "maranhao", "mato grosso", "mato grosso do sul", "minas gerais",
-    "para", "parana", "paraiba", "pernambuco", "piaui", "rio grande do sul", "rio de janeiro", "rio grande do norte", "rondonia",
-    "roraima", "sao paulo", "santa catarina", "sergipe", "tocantins"];
 
-listaAcertos = []
-
-)
 function verificarEstado(mensagem) {
+    
     if (lista.includes(mensagem) && !listaAcertos.includes(mensagem)) {
         listaAcertos.push(mensagem)
-        console.log;
+        console.log(listaAcertos);
         
         return true
     } else {
+        
         return false
     }
-}
+} 
+
+function padronizaçao() {
+
+        const padronizados = {
+            "acre": "Acre",
+            "alagoas": "Alagoas",
+            "amapa": "Amapá",
+            "amazonas": "Amazonas",
+            "bahia": "Bahia",
+            "ceara": "Ceará",
+            "distrito federal": "Distrito Federal",
+            "espirito santo": "Espírito Santo",
+            "goias": "Goiás",
+            "maranhao": "Maranhão",
+            "mato grosso": "Mato Grosso",
+            "mato grosso do sul": "Mato Grosso do Sul",
+            "minas gerais": "Minas Gerais",
+            "para": "Pará",
+            "parana": "Paraná",
+            "paraiba": "Paraíba",
+            "pernambuco": "Pernambuco",
+            "piaui": "Piauí",
+            "rio de janeiro": "Rio de Janeiro",
+            "rio grande do norte": "Rio Grande do Norte",
+            "rio grande do sul": "Rio Grande do Sul",
+            "rondonia": "Rondônia",
+            "roraima": "Roraima",
+            "sao paulo": "São Paulo",
+            "santa catarina": "Santa Catarina",
+            "sergipe": "Sergipe",
+            "tocantins": "Tocantins"
+        };
+
+    };
 
